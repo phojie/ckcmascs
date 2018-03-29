7c42590c0b28b5d77687609af30701739b821fa1
phojie <rengelphojie@yahoo.com>
2018-03-29 23:54:15 +0200

    oe

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,43 +25,45 @@ public function __construct()
      */
     public function index()
     {
-        $gettype=auth::user()->type;
-        $getid=auth::user()->id;
-        $user=DB::table('staff')
-            ->where('user_id',$getid)
-            ->first();
+        // $gettype=auth::user()->type;
+        // $getid=auth::user()->id;
+        // $user=DB::table('staff')
+        //     ->where('user_id',$getid)
+        //     ->first();
 
-        if($gettype == 0){
-            return view('adminfiles.superadmin');
-        }
-        elseif($gettype == -1 || $gettype == 3){
-            return view('registrarfiles.dashregistrar');
-        }
-        elseif($gettype == 2 ){
-            return view('instructorfiles.dashstudents');
-        }
-        elseif($gettype == 1)
-        {
-            if($user->ad==1){
-                return "your account was disable";
-            }
-            elseif($user->ad==0){
+        // if($gettype == 0){
+        //     return view('adminfiles.superadmin');
+        // }
+        // elseif($gettype == -1 || $gettype == 3){
+        //     return view('registrarfiles.dashregistrar');
+        // }
+        // elseif($gettype == 2 ){
+        //     return view('instructorfiles.dashstudents');
+        // }
+        // elseif($gettype == 1)
+        // {
+        //     if($user->ad==1){
+        //         return "your account was disable";
+        //     }
+        //     elseif($user->ad==0){
     
-                // if($gettype==1){
-                    return view('instructorfiles.dashinstructor');
+        //         // if($gettype==1){
+        //             return view('instructorfiles.dashinstructor');
                     
-                // }
-            }
-            else{
-                return "lol";
-            }
+        //         // }
+        //     }
+        //     else{
+        //         return "lol";
+        //     }
             
 
+        // }
+        // else{
+        //     return "lol";
+        // }
+        if($gettype == 1){
+            return view('instructorfiles.dashinstructor');
         }
-        else{
-            return "lol";
-        }
-        
         
     }
 }
